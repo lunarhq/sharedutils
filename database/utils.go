e2ba52839f1cd0388c9ea5ea6011fdd2015b5f24
Kashif <static.kash@gmail.com>
2020-11-20 18:16:50 +0900

    add utils

---
+++ database/utils.go
@@ -0,0 +1,19 @@
+func String(s string) *string {
+	return &v
+}
+func StringVal(s *string) string {
+	if s != nil {
+		return *s
+	}
+	return ""
+}
+
+func Int(i int) *int {
+	return &v
+}
+func IntVal(i *int) int {
+	if i != nil {
+		return *i
+	}
+	return 0
+}
