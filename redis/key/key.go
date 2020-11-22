8bff2583c4b85a1f192f349c201959233f543098
Kashif <static.kash@gmail.com>
2020-11-22 11:30:46 +0900

    complete redis key support

--- redis/key/key.go
+++ redis/key/key.go
@@ -1,6 +1,8 @@
 package key
 
 import (
+	"encoding/json"
+
 	"github.com/go-redis/redis"
 	"github.com/lunarhq/sharedutils/types"
 )
@@ -10,9 +12,31 @@ type Client struct {
 }
 
 func (c *Client) Get(secretToken string) (*types.Key, error) {
-	return nil, nil
+	b, err := c.R.Get("token-" + secretToken).Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	var result types.Key
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
 
 func (c *Client) Store(key types.Key) error {
-	return nil
+	payload, err := json.Marshal(key)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.R.Set("token-"+key.SecretToken, payload, 0).Result()
+	return err
+}
+
+func (c *Client) Delete(secretToken string) error {
+	_, err := c.R.Del("token-" + secretToken).Result()
+	return err
 }
