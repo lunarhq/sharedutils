b72ae6793285ba6515a4a06027cc4e26177e588d
Kashif <static.kash@gmail.com>
2021-04-01 19:28:22 +0900

    update type

--- types/usage.go
+++ types/usage.go
@@ -1,12 +1,8 @@
 package types
 
 type Usage struct {
-	ID         string `json:"id" firestore:"id"`
-	AccountID  string `json:"accountId" firestore:"accountId"`
-	Key        string `json:"key" firestore:"key"`
-	Blockchain string `json:"blockchain" firestore:"blockchain"`
-	Network    string `json:"network" firestore:"network"`
-	IsMainnet  bool   `json:"isMainnet" firestore:"isMainnet"`
-	Date       string `json:"date" firestore:"date"`
-	Hits       int64  `json:"hits" firestore:"hits"`
+	AccountID string `json:"accountId" firestore:"accountId"`
+	Date      string `json:"date" firestore:"date"`
+	Mainnet   int64  `json:"mainnet" firestore:"mainnet"`
+	Testnet   int64  `json:"testnet" firestore:"testnet"`
 }
