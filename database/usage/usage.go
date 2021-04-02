cbf7cfac5e06d83e0dc96835653b199bfa560949
Kashif <static.kash@gmail.com>
2021-04-02 09:51:31 +0900

    add increment method

--- database/usage/usage.go
+++ database/usage/usage.go
@@ -29,3 +29,21 @@ func (c *Client) Get(accId string, date string) (*types.Usage, error) {
 
 	return &item, nil
 }
+func (c *Client) Increment(accId string, date string, networkType string, hits int64) error {
+	//1. First check if key exist else init it
+	key := "api_usage/" + accId + "/items/" + date
+	_, err := c.DB.Doc(key).Get(c.Ctx)
+	if err != nil {
+		initData := map[string]int64{"mainnet": 0, "testnet": 0}
+		_, err = c.DB.Doc(key).Set(c.Ctx, initData)
+		if err != nil {
+			return err
+		}
+	}
+
+	updates := []firestore.Update{
+		firestore.Update{Path: "mainnet", Value: firestore.Increment(hits)},
+	}
+	_, err = c.DB.Doc(key).Update(c.Ctx, updates)
+	return err
+}
